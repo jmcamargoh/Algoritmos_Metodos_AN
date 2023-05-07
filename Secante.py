a988c9268a6adc086ae50b79383e388dabdf93bb
Jose Manuel <jmcamargoh@eafit.edu.co>
2023-05-07 14:21:16 -0500

    Secante con Traducción (y errores)

--- Secante.py
+++ Secante.py
@@ -1,9 +1,30 @@
+import re
 import numpy as np
-from math import *
 from tabulate import tabulate
 
+def reemplazar_funciones_matematicas(expr):
+    # Expresión regular para buscar nombres de funciones matemáticas y operadores matemáticos
+    pattern = r'\b(sin|cos|tan|sqrt|exp|log|log10)|(\*\*|\^|\+|\-|\*|\/)'
+    # Función para reemplazar cada nombre de función y operador matemático
+    def replace(match):
+        # Si es una función matemática, devuelve su versión con prefijo 'numpy.'
+        if match.group(1):
+            return f'np.{match.group(1)}'
+        # Si es el carácter '^', devuelve el operador '**'
+        elif match.group(2) == '^':
+            return '**'
+        # De lo contrario, devuelve el operador o carácter original
+        else:
+            return match.group(2)
+    # Reemplaza los nombres de funciones y operadores en la expresión por sus equivalentes
+    return re.sub(pattern, replace, expr)
+
+expr = 'log(sin(x)^2 + 1)-(1/2)'
+expr_with_numpy = reemplazar_funciones_matematicas(expr)   # Convertir función a string
+func = eval(f"lambda x: {expr_with_numpy}") # Convertir string a función
+
 def f(x):
-    return (abs(x)**(x-1)-2.5*x-5); # función no lineal a evaluar
+    return (func(x)); # función no lineal a evaluar
 
 def secante(f, p_0, p_1, tol, n):
     e_abs = abs(p_1 - p_0);
@@ -32,4 +53,4 @@ def secante(f, p_0, p_1, tol, n):
     if i < n:
         print('Aproximación de la raíz encontrada en x = ', p_2);
 
-secante(f,-3,-2,10**-15,2);
+secante(f,0.5,1,10**-7,100);
