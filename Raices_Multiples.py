50c1fdf7392752bd95aecc45a131945f0b3628cb
Jose Manuel <jmcamargoh@eafit.edu.co>
2023-05-07 14:26:06 -0500

    Raíces Múltiples con Traducción

--- Raices_Multiples.py
+++ Raices_Multiples.py
@@ -1,19 +1,46 @@
-import math
-from math import *
+import re
 import numpy as np
 from tabulate import tabulate
 
+def reemplazar_funciones_matematicas(expr):
+    # Expresión regular para buscar nombres de funciones matemáticas y operadores matemáticos
+    pattern = r'\b(sin|cos|tan|sqrt|exp|log|log10)|(\*\*|\^|\+|\-|\*|\/)'
+    # Función para reemplazar cada nombre de función y operador matemático
+    def replace(match):
+        # Si es una función matemática, devuelve su versión con prefijo 'numpy.'
+        if match.group(1):
+            return f'np.{match.group(1)}'
+        # Si es el carácter '^', devuelve el operador '**'
+        elif match.group(2) == '^':
+            return '**'
+        # De lo contrario, devuelve el operador o carácter original
+        else:
+            return match.group(2)
+    # Reemplaza los nombres de funciones y operadores en la expresión por sus equivalentes
+    return re.sub(pattern, replace, expr)
+
+exprf = 'exp(x)-x-1'
+expr_with_numpy_f = reemplazar_funciones_matematicas(exprf)   # Convertir función a string
+funcf = eval(f"lambda x: {expr_with_numpy_f}") # Convertir string a función
+
+exprdf = 'exp(x)-1'
+expr_with_numpy_df = reemplazar_funciones_matematicas(exprdf)   # Convertir función a string
+funcdf = eval(f"lambda x: {expr_with_numpy_df}") # Convertir string a función
+
+exprd2f = 'exp(x)'
+expr_with_numpy_d2f = reemplazar_funciones_matematicas(exprd2f)   # Convertir función a string
+funcd2f = eval(f"lambda x: {expr_with_numpy_d2f}") # Convertir string a función
 
 def f(x):
-  return (np.exp(x) - x - 1);   # es igual a = e^x-x-1
+  return (funcf(x));  
 
 def df(x):
-  return (np.exp(x) - 1);
+  return (funcdf(x));
 
 def d2f(x):
-  return (np.exp(x));
+  return (funcd2f(x));
 
-def root(f,df,d2f,x0,tol,n):
+def raices_multiples(f,df,d2f,x0,tol,n):
   xant = x0;
   fant = f(xant);
   e_abs=1000;
@@ -37,4 +64,4 @@ def root(f,df,d2f,x0,tol,n):
     print("Solution not found for tolerance = ", tol);
   print(tabulate(resultados, headers=["Iteraciones", "Xi", "f(x)", "Error"], tablefmt="github"))
 
-root(f,df,d2f,1,10**-5,100);
+raices_multiples(f,df,d2f,1,10**-7,100);
