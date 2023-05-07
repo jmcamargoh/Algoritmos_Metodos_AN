4471f59c1587b4315a3afa83d0ba1a77733c89cd
Jose Manuel <jmcamargoh@eafit.edu.co>
2023-05-07 13:10:28 -0500

    Punto Fijo con Traducción

--- Punto_Fijo.py
+++ Punto_Fijo.py
@@ -1,14 +1,37 @@
-import math
-from math import *
+import re
 import numpy as np
 from tabulate import tabulate
 
+def reemplazar_funciones_matematicas(expr):
+    # Expresión regular para buscar nombres de funciones matemáticas y operadores matemáticos
+    pattern = r'\b(sin|cos|tan|sqrt|exp|log|log10)|(\*\*|\^|\+|\-|\*|\/)'
+    # Función para reemplazar cada nombre de función y operador matemático
+    def replace(match):
+        # Si es una función matemática, devuelve su versión con prefijo 'numpy.'
+        if match.group(1):
+            return f'np.{match.group(1)}'
+        # Si es el carácter '^', devuelve el operador '**'
+        elif match.group(2) == '^':
+            return '**'
+        # De lo contrario, devuelve el operador o carácter original
+        else:
+            return match.group(2)
+    # Reemplaza los nombres de funciones y operadores en la expresión por sus equivalentes
+    return re.sub(pattern, replace, expr)
+
+exprf = 'log(sin(x)^2 + 1)-(1/2)-x'
+expr_with_numpy_f = reemplazar_funciones_matematicas(exprf)   # Convertir función a string
+funcf = eval(f"lambda x: {expr_with_numpy_f}") # Convertir string a función
+
+exprg = 'log(sin(x)^2 + 1)-(1/2)'
+expr_with_numpy_g = reemplazar_funciones_matematicas(exprg)   # Convertir función a string
+funcg = eval(f"lambda x: {expr_with_numpy_g}") # Convertir string a función
 
 def f1(x):
-    return ((-math.e**-x)-x**2-5*x-20);    #función
+    return (funcf(x));    #función
 
 def g(x):
-    return (math.sqrt((-math.e**-x)+5*x+20));  # evaluación
+    return (funcg(x));  # evaluación
 
 def punto_fijo(x0, tol, itermax):
     iter = 0
@@ -26,4 +49,4 @@ def punto_fijo(x0, tol, itermax):
 
     print(tabulate(resultados, headers=["Iteraciones", "Xi", "g(xi)", "f(x)", "Error"], tablefmt="github", floatfmt=(".10f",".10f",".10f")))
 
-punto_fijo(7, 10**-3, 100)
+punto_fijo(-0.5, 10**-7, 100)
