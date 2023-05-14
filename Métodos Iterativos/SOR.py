4f385e711101bb9691e7b9cbd257fd5cdaf07367
Jose Manuel <jmcamargoh@eafit.edu.co>
2023-05-14 18:55:11 -0500

    Método de SOR

---
+++ Métodos Iterativos/SOR.py
@@ -0,0 +1,48 @@
+import numpy as np
+from tabulate import tabulate
+
+def sor_method(A,b,x0,Tol,Niter,w):
+    c=0
+    error = Tol+1
+    D = np.diag(np.diag(A))
+    L = -np.tril(A, -1)
+    U = -np.triu(A, 1)
+    tabla = []
+    while error > Tol and c < Niter:
+        T = np.linalg.inv(D-w*L)@((1-w)*D+w*U)
+        C = w*np.linalg.inv(D-w*L)@b
+        x1 = T@x0+C
+        E = (np.linalg.norm(x1-x0, ord=np.inf))
+        error = E
+        tabla.append([c] + list(x0) + [E])
+        x0 = x1
+        c += 1
+    if error < Tol:
+        s = x0
+        print(f"La aproximación de la solución del sistema con una tolerancia = {Tol} es: ")
+        print(s)
+    else:
+        s = x0
+        print(f"Fracasó en {Niter} iteraciones")
+    
+    tabla.append([c] + list(x0) + [E]) 
+    headers = ['Iteración'] + [f'x{i+1}' for i in range(len(x0))] + ['Error']
+    print(tabulate(tabla, headers=headers, floatfmt=".8f", tablefmt="grid"))
+    return (E,s)
+
+
+A = np.array([[11, 5, 4],
+              [5, 25, 4],
+              [5, 4, 10]])
+
+b = np.array([10, 10, 10])
+
+x0 = np.array([1, 1, 1])
+
+Tol = 1e-5
+
+Niter = 100
+
+w = 1.5
+
+sor_method(A, b, x0, Tol, Niter, w)
