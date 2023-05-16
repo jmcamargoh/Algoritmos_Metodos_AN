1010a4dd115a3e5972e7e20526dff0fa22d1dca9
Jose Manuel <jmcamargoh@eafit.edu.co>
2023-05-15 20:10:27 -0500

    Spline Lineal con Gráfico

--- Interpolación/Spline.py
+++ Interpolación/Spline.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sym
+import matplotlib.pyplot as plt 
 
 def Spline(xi, fi, d):
     n = len(xi)
@@ -18,6 +19,28 @@ def Spline(xi, fi, d):
         for i in range(1,n,1):      # Salen al revés, para evitar todo el problema que tuve con Diferencias
             px = tabla_px[i-1]
             print(px)
+        
+        # Gráfica
+        xcoordenadas = np.array([])
+        ycoordenadas = np.array([])
+        for seccion in range(1,n,1):    # Recorre cada sección del trazador
+            a = xi[seccion-1]       # A y B para cada sección del trazador (si no se hace, quedan las fuciones completas e infinitas)
+            b = xi[seccion]
+            xseccion = np.linspace(a,b)     # Puntos equiespaciados entre a y b
+            pxseccion = tabla_px[seccion-1]     # La función actual del trazador (en esa sección)
+            pxt = sym.lambdify(x,pxseccion)     # Convertir a función
+            yseccion = pxt(xseccion)            # Evaluación en Y
+            xcoordenadas = np.concatenate((xcoordenadas,xseccion))      # Se agregan los puntos anteriores a los arreglos de las coordenadas para que el programa grafique
+            ycoordenadas = np.concatenate((ycoordenadas,yseccion))
+
+        plt.plot(xi,fi, 'ro', label='puntos')
+        plt.plot(xcoordenadas,ycoordenadas, label='trazador')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend()
+        plt.title("Gráfico de Trazador Lineal")
+        plt.grid(True)
+        plt.show()
 
 
 x = [-2, -1, 2, 3]
